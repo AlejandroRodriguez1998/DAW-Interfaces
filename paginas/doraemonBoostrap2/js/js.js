4be655e15c5637bee65acab6cea1942e4fe04802
Alejandro <43789179+AlejandroRodriguez1998@users.noreply.github.com>
2024-02-10 02:17:53 +0100

    Add files via upload

---
+++ paginas/doraemonBoostrap2/js/js.js
@@ -0,0 +1,9 @@
+$('#animatedElement').mouseover(function() {
+    $(this).addClass("bigEntrance");
+});
+$('#animatedElement1').mouseover(function() {
+    $(this).addClass("bigEntrance");
+});
+$('#animatedElement2').mouseover(function() {
+    $(this).addClass("bigEntrance");
+});
---
+++ paginas/examenBoostrap/js/mi_js.js
@@ -0,0 +1,10 @@
+/* 
+ * Autor:
+ * Fecha: 2019
+ * Módulo: DIW
+ */
+
+$('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
